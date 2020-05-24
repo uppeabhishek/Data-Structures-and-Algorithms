67a90d67529a903d0310bc839ec1a51ab888986c
Abhishek <abhishek.uppe@ibigroup.com>
2020-05-24 17:56:58 +0530

    added sort character

---
+++ python/SortCharactersByFrequency.py
@@ -0,0 +1,19 @@
+from collections import defaultdict;
+class Solution:
+    def frequencySort(self, s: str) -> str:
+        d = defaultdict(int);
+        
+        for c in s:
+            d[c]+=1;
+        
+        
+        d = sorted(d.items(), key=lambda kv: (kv[1]), reverse=True)
+        
+        res = "";
+        
+        
+        for ele in d:
+            for i in range(ele[1]):
+                res+=ele[0];
+                
+        return (res);
